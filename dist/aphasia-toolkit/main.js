483af77d54462e756942e9a3192dd7ac35a5d3a9
Kamil Chlebek <kchlebek@keldoc.com>
2023-04-02 16:16:51 +0200

    feat: highlight currently playing letter

--- dist/aphasia-toolkit/main.js
+++ dist/aphasia-toolkit/main.js
@@ -1,7 +1,17 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll('button')
+const table = document.querySelector('table')
 buttons.forEach(button => {
+    const tr = button.closest('tr')
     const audio = new Audio(`assets/${button.dataset.letter}.ogg`)
-    button.addEventListener("click", () => {
-        audio.play();
-    });
-});
+    button.addEventListener('click', () => {
+        audio.play()
+    })
+    audio.addEventListener('play', () => {
+        tr.classList.add('playing')
+        table.classList.add('playing')
+    })
+    audio.addEventListener('ended', () => {
+        tr.classList.remove('playing')
+        table.classList.remove('playing')
+    })
+})
